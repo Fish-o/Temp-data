e06d7929048771a98ff62e5ddd5af04561dc9fa9
Fish <64684124+Fish-o@users.noreply.github.com>
2023-01-07 20:38:12 +0100

    Update x2.js

--- assets/js/x2.js
+++ assets/js/x2.js
@@ -2,5 +2,9 @@ function main_func(){
   console.log("Hallo vanaf een js programma gehost van het internet");
   console.log("Dit is mijn origin: "+window.origin);
   console.log("Dit is mijn domain: "+document.domain);
+  console.log("Kijken of ik de pagina ook aan kan passen..")
+  const el = document.getElementById("inbox-counter");
+  console.log("Got element:", el);
+  el.innerHtml = "k dat was makkelijk";
 }
 main_func()
