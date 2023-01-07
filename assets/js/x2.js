c7b1a4883fb8d1385b3422efd6e2acc8390fb6ef
Fish <64684124+Fish-o@users.noreply.github.com>
2023-01-07 20:25:40 +0100

    Update x2.js

--- assets/js/x2.js
+++ assets/js/x2.js
@@ -1,4 +1,6 @@
 function main_func(){
   console.log("Hallo vanaf een js programma gehost van het internet");
+  console.log("Dit is mijn origin: "+window.origin);
+  console.log("Dit is mijn domain: "+document.domain);
 }
 main_func()
